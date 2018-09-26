c49598e196c9543848a2a72b0b644e602d82bac7
Juan Carlos <jcgamar@unal.edu.co>
2018-09-26 13:20:35 -0500

    esqueleto vista principal

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -6,21 +6,77 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
 
-Hello.defaultProps = {
-  name: 'David'
-}
+class Root extends React.Component {
+  state = {
+    input: "",
+    output: "",
+  }
 
-Hello.propTypes = {
-  name: PropTypes.string
+  constructor(props) {
+    super(props)
+    
+    this.listenerInput = this.listenerInput.bind(this)
+    this.listenerOutput = this.listenerOutput.bind(this)
+    this.listenerConsultar = this.listenerConsultar.bind(this)
+    this.listenerClear = this.listenerClear.bind(this)
+  }
+
+  listenerInput(e) {
+    this.setState({ input: e.target.value })
+  }
+
+  listenerOutput(e) {
+    this.setState({ output: e.target.value })
+  }
+
+  listenerConsultar(e){
+    if(this.state.input.trim() == "" && this.state.output.trim() == ""){
+      alert("Ingrese un input")
+      return;
+    }
+
+    if(this.state.input.trim() != ""){
+      this.setState({output: "la salida acá"})
+      return;
+    }
+    
+    if(this.state.output.trim() != ""){
+      this.setState({input: "la entrada acá"})
+      return;
+    }
+
+  }
+  listenerClear(){
+    this.setState({input: "", output: ""})
+  }
+
+  render() {
+    return (
+      <div>
+        <label>
+          Input:
+        <textarea name="input" onChange={this.listenerInput} value={this.state.input} />
+        </label>
+        <label>
+          Output:
+        <textarea name="output" onChange={this.listenerOutput} value={this.state.output} />
+        </label>
+        <button onClick={this.listenerConsultar}>
+          Consultar
+        </button>
+        <button onClick={this.listenerClear}>
+          Limpiar
+        </button>
+      </div>
+    )
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="Juan" />,
-    document.body.appendChild(document.createElement('div')),
+    <Root />,
+    document.getElementById("home-index")
+    // document.body.appendChild(document.createElement('div')),
   )
 })
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'des/cifrar'
+  get 'des/descifrar'
+  get 'des/cifrar'
+  get 'des/decifrar'
   root 'home#index'
 end
