45a62cb5a682f8659ca2d208c5f7693efc95c1fd
Diego Araque <daaraquem@unal.edu.co>
2018-11-26 17:24:12 -0500

    prueba algoritmo
    se genera algoritmos para generacion de claves, pendientes pruebas de encriptado y descifrado

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'cipher_text', to: 'home#consultar_cipher'
   get 'firma', to: 'home#signature'
+  
+  get 'joli', to: 'claves#selector'
 end
---
+++ test/controllers/claves_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ClavesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
