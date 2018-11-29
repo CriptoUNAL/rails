8b677e86f9a7488f499e3b7490349b6036d4a420
Iván Herrera <biherrerap@unal.edu.co>
2018-11-29 16:08:03 -0500

    Código signup front

--- app/javascript/packs/signup.jsx
+++ app/javascript/packs/signup.jsx
@@ -8,6 +8,42 @@ import ReactDOM from 'react-dom'
 
 class Root extends React.Component {
 
+        state = {
+          mensaje: "",
+          id: false
+      }
+      constructor(props) {
+          super(props)
+
+          this.listenerSignUp = this.listenerSignUp.bind(this)
+      }
+
+      componentWillMount() {
+      }
+
+      listenerSignUp(e){
+        
+        if (document.getElementById("inputUserame") != null && document.getElementById("inputPassword") != null){
+          const payload = { user: document.getElementById("inputUserame").value, pass: document.getElementById("inputPassword").value }
+
+          //alert(document.getElementById("inputEmail").value);
+          fetch(`register`, {
+              method: "post",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify(payload)
+          })
+              .then(resp => resp.json())
+              //.catch(_ => alert( "Error en el proceso" ))
+              .then(data => console.log(data))
+          setTimeout(alert("Al crear un usuario, se aceptan los terminos y condiciones"),4000);
+
+      }
+          
+
+
+      }
+      
+
 
   render() {
 
@@ -15,36 +51,34 @@ class Root extends React.Component {
 
     return (
       <div>
+        <div class = "container" id = "flo">
         <div className="row">
-                <div className="col-lg-10 col-xl-9 mx-auto">
+        <div className="col-lg-2">
+          .
+        </div>
+                <div id = "feto" className="col-lg-8">
                     <div className="card card-signin flex-row my-5">
-                        <div className="card-img-left d-none d-md-flex">
-                        </div>
                         <div className="card-body">
-                            <h5 className="card-title text-center">Regístrate</h5>
+                            <h2 className="card-title text-center"><strong>Regístrate</strong></h2>
                             <form className="form-signin">
                                 <div className="form-label-group">
                                     <input type="text" id="inputUserame" className="form-control" placeholder="Username" required autoFocus></input>
                                     <label htmlFor="inputUserame">Usuario</label>
                                 </div>
                                 <div className="form-label-group">
-                                    <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required></input>
-                                    <label htmlFor="inputEmail">Email </label>
-                                </div>
-                                <div className="form-label-group">
                                     <input type="password" id="inputPassword" className="form-control" placeholder="Password" required></input>
                                     <label htmlFor="inputPassword">Contraseña</label>
                                 </div>
-                                <div className="form-label-group">
-                                    <input type="password" id="inputConfirmPassword" className="form-control" placeholder="Password" required></input>
-                                    <label htmlFor="inputConfirmPassword">Confirmar contraseña</label>
-                                </div>
-                                <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit" onClick={this.listenerSignUp} > >Registrarme</button>
-                                <a  className="d-block text-center mt-2 small" href="/sign_in" target="_self">Iniciar sesión</a>
+                                   <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit" onClick={this.listenerSignUp} > Login </button>
+                                   <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit" onClick={this.listenerSignUp} > Sign Up</button>
                             </form>
                         </div>
                     </div>
                 </div>
+                <div className="col-lg-2">
+                 .
+                  </div>
+            </div>
             </div>
       </div>
     )
